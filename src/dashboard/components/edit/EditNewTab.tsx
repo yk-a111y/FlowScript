43784ac931fb161e64dee08e59902a98c542be0c
yk-a111y <yk19908011@163.com>
2025-01-24 20:54:48 +0800

    feat: add new Ui Comp

--- src/dashboard/components/edit/EditNewTab.tsx
+++ src/dashboard/components/edit/EditNewTab.tsx
@@ -1,6 +1,7 @@
-import { Textarea } from '@/components/ui/UiTextarea';
 import { useState } from 'react';
-
+import { Textarea } from '@/components/ui/UiTextarea';
+import { Label } from '@/components/ui/UiLabel';
+import { Input } from '@/components/ui/UiInput';
 interface EditNewTabProps {
   compData: any;
   updateBlockData: (key: string, value: string) => void;
@@ -8,6 +9,7 @@ interface EditNewTabProps {
 
 const EditNewTab = ({ compData, updateBlockData }: EditNewTabProps) => {
   const [description, setDescription] = useState(compData.data.description);
+  const [newTabUrl, setNewTabUrl] = useState(compData.data.url);
   return (
     <div>
       <Textarea
@@ -16,6 +18,15 @@ const EditNewTab = ({ compData, updateBlockData }: EditNewTabProps) => {
         onChange={(e) => {
           setDescription(e.target.value);
           updateBlockData('description', e.target.value);
+        }}
+      />
+      <Label>New Tab URL</Label>
+      <Input
+        value={newTabUrl}
+        placeholder="https://www.test.com"
+        onChange={(e) => {
+          setNewTabUrl(e.target.value);
+          updateBlockData('url', e.target.value);
         }}
       />
     </div>
