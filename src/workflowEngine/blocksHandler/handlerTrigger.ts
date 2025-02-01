3ac4062da09398104bcfff4ef2f98ade1376cfd8
yk-a111y <yk19908011@163.com>
2025-02-01 23:59:38 +0800

    feat: trigger handler

--- src/workflowEngine/blocksHandler/handlerTrigger.ts
+++ src/workflowEngine/blocksHandler/handlerTrigger.ts
@@ -1,6 +1,5 @@
 async function trigger(block) {
   return new Promise((resolve) => {
-    console.log('🚀 ~ handleTrigger');
     resolve({
       data: '',
       nextBlockId: this.getBlockConnections(block.id),
