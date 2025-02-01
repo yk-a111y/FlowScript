db054f0c715e3fdae306cef58a91f095271cb71c
yk-a111y <yk19908011@163.com>
2025-02-02 00:19:38 +0800

    feat: newtab handler

--- src/workflowEngine/blocksHandler/handlerNewTab.ts
+++ src/workflowEngine/blocksHandler/handlerNewTab.ts
@@ -1,9 +1,32 @@
-async function newTab(block) {
+import browser from 'webextension-polyfill';
+
+async function newTab({ id, data }) {
+  console.log('🚀 ~ newTab ~ data:', data, id);
+
+  let tab = null;
+
+  if (data.updatePrevTab && this.activeTab.id) {
+    tab = await browser.tabs.update(this.activeTab.id, {
+      url: data.url,
+      active: data.active,
+    });
+  } else {
+    tab = await browser.tabs.create({
+      url: data.url,
+      active: data.active,
+      windowId: this.windowId,
+    });
+  }
+
+  this.activeTab.url = data.url;
+
+  await browser.windows.update(tab.windowId, { focused: true });
+
   return new Promise((resolve) => {
     console.log('🚀 ~ handlerNewtab');
     resolve({
       data: '',
-      nextBlockId: this.getBlockConnections(block.id),
+      nextBlockId: this.getBlockConnections(id),
     });
   });
 }
